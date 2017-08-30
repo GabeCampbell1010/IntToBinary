286b574603b7ecd2b9d2be3d7b6eed1ee5be0d54
u0861355 <u0861355@utah.edu>
2017-08-30 16:24:23 -0600

    worked?

---
+++ IntToBinary/IntToBinary/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetBinaryValue(46));
+            Console.ReadKey();
+        }
+        public static string GetBinaryValue(int value)
+        {
+            //THIS VAR ANS IS SIMPLY USED TO FIND THE INTTOBINARY CONVERSION BUILTIN TO VS TO USE TO COMPArE TO THE ANSWER IN THE DEBUG.ASSERT STATEMENT, THE ACTUAL ANSWER IS NOT BEING DETERMINED THIS WAY
+            var ans = Convert.ToString(value, 2);
+            //END DEBUG SECTION
+
+            int quotient = 1;
+            int remainder;
+            List<int> list = new List<int>();
+            List<int> listReversed = new List<int>();
+            string answer = "";
+
+            while (quotient != 0)
+            {
+                quotient = value / 2;
+                remainder = value % 2;
+                list.Add(remainder);
+                value = quotient;
+            }
+
+            //reverse the list to make it in the correct order
+            for (int i = 0; i < list.Count(); i++)
+            {
+                listReversed.Add(list[list.Count() - 1 - i]);
+            }
+            list = listReversed;
+
+            foreach (int num in list) answer += num.ToString();
+
+            //begin debug section
+            Debug.Assert(answer == ans);
+            //end debug section
+
+            return answer;  
+        }
+    }
+}
