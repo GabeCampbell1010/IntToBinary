46bc12894bf09758f56b4370bd68a8defe902c57
GabeCampbell1010 <gabecampbell1010@gmail.com>
2017-09-02 01:08:33 -0600

    added the bitwise conversion method

--- IntegerToBinary/IntegerToBinary/Program.cs
+++ IntegerToBinary/IntegerToBinary/Program.cs
@@ -11,7 +11,7 @@ namespace IntegerToBinary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(GetBinaryValue(46));
+            Console.WriteLine(GetBinaryValue(11));
             Console.ReadKey();
         }
         public static string GetBinaryValue(int value)
@@ -20,11 +20,32 @@ namespace IntegerToBinary
             var ans = Convert.ToString(value, 2);
             //END DEBUG SECTION
 
+            //variables for the modulus method
             int quotient = 1;
             int remainder;
             List<int> list = new List<int>();
             List<int> listReversed = new List<int>();
             string answer = "";
+
+            //variables for the bitwise method
+            List<int> listExperiment = new List<int>();
+            List<int> listExperimentReversed = new List<int>();
+            int valueExperiment = value;
+            int bit;
+
+            Console.WriteLine("Intitial Integer Value: " + value);
+
+            //populate the list with the correct integer values as per the necessary calculations
+            //can i just exclude the below part and replace it with bitwise operators?
+
+
+            while (valueExperiment > 0)
+            {
+                bit = valueExperiment & 1;
+                listExperiment.Add(bit);
+                valueExperiment >>= 1;
+            }
+
 
             while (quotient != 0)
             {
@@ -41,10 +62,27 @@ namespace IntegerToBinary
             }
             list = listReversed;
 
+            for (int i = 0; i < listExperiment.Count(); i++)
+            {
+                listExperimentReversed.Add(listExperiment[listExperiment.Count() - 1 - i]);
+            }
+            listExperiment = listExperimentReversed;
+
+            //assert
+            string debuganswer = "";
+            foreach (int num in listExperiment) debuganswer += num.ToString();
+            Debug.Assert(debuganswer == ans);
+
+            
+
+            answer = "Modulus Conversion Method Answer: ";
             foreach (int num in list) answer += num.ToString();
+            answer += "\nBitwise Operator Conversion Method Answer: ";
+            foreach (int num in listExperiment) answer += num.ToString();
+
 
             //begin debug section
-            Debug.Assert(answer == ans);
+            
             //end debug section
 
             return answer;
